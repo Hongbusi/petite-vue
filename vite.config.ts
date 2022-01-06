315bedd8c55b37d4b6a6a915c7846bed4043dca6
Hongbusi <q951686662@qq.com>
2022-01-06 18:22:27 +0800

    chore: remove @

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
   plugins: [vue()]
 })
