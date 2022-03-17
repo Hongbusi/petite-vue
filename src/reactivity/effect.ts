5a6187942b6735f57dfa66e0a3324df0253964eb
Hongbusi <q951686662@qq.com>
2022-03-17 22:08:34 +0800

    chore: should return runner when call effect

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -9,7 +9,7 @@ class ReactiveEffect {
 
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
 }
 
@@ -38,4 +38,6 @@ export function trigget(target, key) {
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn)
   _effect.run()
+
+  return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -2,7 +2,7 @@ import { reactive } from '../reactive'
 import { effect } from '../effect'
 
 describe('effect', () => {
-  it('test', () => {
+  it('should observe basic properties', () => {
     let dummy
     const counter = reactive({ num: 0 })
     effect(() => (dummy = counter.num))
@@ -10,5 +10,18 @@ describe('effect', () => {
     expect(dummy).toBe(0)
     counter.num = 7
     expect(dummy).toBe(7)
+  })
+
+  it('should return runner when call effect', () => {
+    let foo = 10
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+
+    expect(foo).toBe(11)
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe('foo')
   })
 })
