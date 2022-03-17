179f9cf9085b5722e2e71209bf51b0522515a971
Hongbusi <q951686662@qq.com>
2022-03-17 13:17:28 +0800

    chore: reset

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './index.css';
-
-const app = createApp(App);
-
-app.mount('#app');
--- src/reactivity/reactive.js
+++
@@ -1,8 +0,0 @@
-export function reactive(target) {
-  if (typeof target !== 'object') {
-    console.warn(`reactive ${target} 必须是一个对象`);
-    return target;
-  }
-
-  return new Proxy(target, mutableHandlers);
-}
