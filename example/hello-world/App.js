6a962922f3b353929edccff2b20b8ad568a044e7
Hongbusi <q951686662@qq.com>
2022-03-24 22:43:29 +0800

    feat(runtime-core): mountElement

--- example/hello-world/App.js
+++ example/hello-world/App.js
@@ -2,7 +2,13 @@ import { h } from '../../lib/petite-vue.esm.js'
 
 export const App = {
   render() {
-    return h('div', `Hello, ${this.message}`)
+    return h(
+      'div', {
+        id: 'root',
+        class: ['red', 'bg-green']
+      },
+      [h('p', { class: 'red' }, 'Hello World'), h('p', { class: 'green' }, 'Hello World')]
+    )
   },
 
   setup() {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from '../shared'
 import { createComponentInstance, setupComponent } from './component'
 
 export function render(vnode, container) {
@@ -5,12 +6,43 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  processElement(vnode, container)
-
-  processComponent(vnode, container)
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  }
+  else if (isObject(vnode.type)) {
+    processComponent(vnode, container)
+  }
 }
 
 function processElement(vnode, container) {
+  mountElement(vnode, container)
+}
+
+function mountElement(vnode, container) {
+  const el = document.createElement(vnode.type)
+
+  const { children } = vnode
+
+  if (typeof children === 'string') {
+    el.textContent = children
+  }
+  else if (Array.isArray(children)) {
+    mountChildren(vnode, el)
+  }
+
+  const { props } = vnode
+  for (const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+
+  container.append(el)
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container)
+  })
 }
 
 function processComponent(vnode, container) {
