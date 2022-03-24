92a394629d39c4d92ce2d3efe1781318ba8eca3a
Hongbusi <q951686662@qq.com>
2022-03-24 21:30:22 +0800

    feat(reactivity): computed

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -3,7 +3,7 @@ import { extend } from '../shared'
 let activeEffect
 let shouldTrack = false
 
-class ReactiveEffect {
+export class ReactiveEffect {
   private _fn: any
   deps = []
   active = true
