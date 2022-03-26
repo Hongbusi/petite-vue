81ff7c173c72877e4934c42e6691fb2822ba8cc9
Hongbusi <q951686662@qq.com>
2022-03-26 22:08:05 +0800

    chore: update

--- example/current-instance/App.js
+++ example/current-instance/App.js
@@ -1,22 +1,9 @@
-import { h, createTextVNode } from '../../lib/petite-vue.esm.js'
+import { h } from '../../lib/petite-vue.esm.js'
 import { Foo } from './Foo.js'
 
 export const App = {
   render() {
-    const app = h('div', {}, 'App')
-    const foo = h(
-      Foo,
-      {},
-      {
-        header: ({ age }) => [
-          h('p', {}, `header${age}`),
-          createTextVNode('hello world')
-        ],
-        footer: () => h('p', {}, 'footer')
-      }
-    )
-
-    return h('div', {}, [app, foo])
+    return h('div', {}, [h('p', {}, 'currentInstance demo'), h(Foo)])
   },
 
   setup() {
--- example/current-instance/Foo.js
+++ example/current-instance/Foo.js
@@ -1,4 +1,4 @@
-import { h, renderSlots } from '../../lib/petite-vue.esm.js'
+import { h } from '../../lib/petite-vue.esm.js'
 
 export const Foo = {
   setup() {
@@ -8,11 +8,6 @@ export const Foo = {
   render() {
     const foo = h('p', {}, 'Foo')
 
-    console.log(this.$slots)
-    return h('div', {}, [
-      renderSlots(this.$slots, 'header', { age: 123 }),
-      foo,
-      renderSlots(this.$slots, 'footer')
-    ])
+    return h('div', {}, [foo])
   }
 }
