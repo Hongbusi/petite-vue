ca89d1a8f3e4448c89d48a51ac5a1890a7ece9b5
Hongbusi <q951686662@qq.com>
2022-03-27 22:49:41 +0800

    feat(example): update

--- example/update/App.js
+++ example/update/App.js
@@ -10,19 +10,45 @@ export const App = {
       count.value++
     }
 
+    const props = ref({
+      foo: 'foo',
+      bar: 'bar'
+    })
+
+    const onChangePropsDemo1 = () => {
+      props.value.foo = 'new foo'
+    }
+
+    const onChangePropsDemo2 = () => {
+      props.value.foo = undefined
+    }
+
+    const onChangePropsDemo3 = () => {
+      props.value.foo = {
+        foo: 'foo'
+      }
+    }
+
     return {
       count,
-      onClick
+      props,
+      onClick,
+      onChangePropsDemo1,
+      onChangePropsDemo2,
+      onChangePropsDemo3
     }
   },
 
   render() {
     return h(
       'div',
-      { id: 'root' },
+      { id: 'root', ...this.props },
       [
         h('div', {}, `count: ${this.count}`),
-        h('button', { onClick: this.onClick }, 'click')
+        h('button', { onClick: this.onClick }, 'click'),
+        h('button', { onClick: this.onChangePropsDemo1 }, 'change props - 修改'),
+        h('button', { onClick: this.onChangePropsDemo2 }, 'change props - undefined'),
+        h('button', { onClick: this.onChangePropsDemo3 }, 'change props - 删除')
       ]
     )
   }
