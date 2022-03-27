119946c9bbf5c28135a1c2d8404b12d06e5af565
Hongbusi <q951686662@qq.com>
2022-03-27 22:22:14 +0800

    feat(reactivity): export ref proxyRefs

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -1,3 +1,4 @@
+import { proxyRefs } from '../reactivity'
 import { shallowReadonly } from '../reactivity/reactive'
 import { emit } from './componentEmit'
 import { initProps } from './componentProps'
@@ -48,7 +49,7 @@ function setupStatefulComponent(instance) {
 
 function handleSetupResult(instance, setupResult) {
   if (typeof setupResult === 'object') {
-    instance.setupState = setupResult
+    instance.setupState = proxyRefs(setupResult)
   }
 
   finishComponentSetup(instance)
