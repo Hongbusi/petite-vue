6729097d7ef7bbf0e8fb499d669cc23cd4d2c6a8
Hongbusi <q951686662@qq.com>
2022-04-03 23:52:27 +0800

    feat(compiler-core): codegen

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,8 +1,12 @@
-export function transform(root, options) {
+export function transform(root, options = {}) {
   const context = createTransformContext(root, options)
-
-  // 遍历 - 深度优先搜索
   traverseNode(root, context)
+
+  createRootCodegen(root)
+}
+
+function createRootCodegen(root) {
+  root.codegenNode = root.children[0]
 }
 
 function createTransformContext(root: any, options: any) {
