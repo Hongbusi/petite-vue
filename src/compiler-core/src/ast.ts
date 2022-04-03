748e9a14570a8fe6d805a8c5a44904861b9463d0
Hongbusi <q951686662@qq.com>
2022-04-03 21:39:44 +0800

    feat(compiler-core): parseElement

--- src/compiler-core/src/ast.ts
+++ src/compiler-core/src/ast.ts
@@ -1,4 +1,5 @@
 export const enum NodeTypes {
   INTERPOLATION = 'interpolation',
-  SIMPLE_EXPRESSION = 'simple_expression'
+  SIMPLE_EXPRESSION = 'simple_expression',
+  ELEMENT = 'element'
 }
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -14,4 +14,14 @@ describe('compiler-core/parse', () => {
       })
     })
   })
+
+  describe('element', () => {
+    it('simple element div', () => {
+      const ast = baseParse('<div></div>')
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: 'div'
+      })
+    })
+  })
 })
