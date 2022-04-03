3c24747467eb28a147f08026912fb4815cf26a37
Hongbusi <q951686662@qq.com>
2022-04-03 16:23:49 +0800

    feat(runtime-core): patchChildren

--- example/patch-children/arrayToArray.js
+++ example/patch-children/arrayToArray.js
@@ -86,15 +86,65 @@ import { h, ref } from '../../lib/petite-vue.esm.js'
 // c (a b)
 // (a b)
 
+// const prevChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'C' }, 'C')
+// ]
+
+// const nextChildren = [
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'C' }, 'C')
+// ]
+
+// 5. 对比中间部分，删除老的（在老的里面存在，新的里面不存在）
+
+// 5.1
+// a b (c d) f g
+// a b (e c) f g
+// D 节点在新的里面是没有的 -- 删除
+// C 节点 props 发生了变化
+
+// const prevChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'C', id: 'c-prev' }, 'C'),
+//   h('p', { key: 'D' }, 'D'),
+//   h('p', { key: 'F' }, 'F'),
+//   h('p', { key: 'G' }, 'G')
+// ]
+
+// const nextChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'E' }, 'E'),
+//   h('p', { key: 'C', id: 'c-next' }, 'C'),
+//   h('p', { key: 'F' }, 'F'),
+//   h('p', { key: 'G' }, 'G')
+// ]
+
+// 5.1.1
+// a b (c e d) f g
+// a b (e c) f g
+// 中间部分，老的比新的多，那么多出来的直接可以干掉（优化删除逻辑）
+
 const prevChildren = [
   h('p', { key: 'A' }, 'A'),
   h('p', { key: 'B' }, 'B'),
-  h('p', { key: 'C' }, 'C')
+  h('p', { key: 'C', id: 'c-prev' }, 'C'),
+  h('p', { key: 'E' }, 'E'),
+  h('p', { key: 'D' }, 'D'),
+  h('p', { key: 'F' }, 'F'),
+  h('p', { key: 'G' }, 'G')
 ]
 
 const nextChildren = [
+  h('p', { key: 'A' }, 'A'),
   h('p', { key: 'B' }, 'B'),
-  h('p', { key: 'C' }, 'C')
+  h('p', { key: 'E' }, 'E'),
+  h('p', { key: 'C', id: 'c-next' }, 'C'),
+  h('p', { key: 'F' }, 'F'),
+  h('p', { key: 'G' }, 'G')
 ]
 
 export default {
